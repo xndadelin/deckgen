5eb71539182eb79b03e73c75115b084462e43d0b
xndadelin <adelinmihai071@gmail.com>
2025-10-22 16:25:42 +0300

    Improve design elements.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import authenticateUser from "@/utils/auth/main";
-import { Button, Container, Text, Group } from "@mantine/core";
+import { Button, Container, Text, Group, Stack, Title, Badge, Anchor } from "@mantine/core";
 
 export default function Home() {
   return (
@@ -11,55 +11,78 @@ export default function Home() {
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        height: "100vh",
+        minHeight: '100dvh',
+        textAlign: 'center'
       }}
       size={"md"}
     >
-      <Text
-        style={{
-          fontSize: 52,
-          textAlign: "center",
-        }}
-        fw={700}
-      >
-        A web platform that leverages{" "}
-        <span
+      <Stack gap={"xl"} align="center" ta="center">
+        <Badge size="lg" variant="light" color="blue" radius="sm">
+          Flashcards powered by AI!
+        </Badge>
+        <Title
+          order={1}
           style={{
-            display: "inline-block",
-            fontWeight: "bold",
-            fontSize: 60,
-            background:
-              "linear-gradient(90deg, #4c9aff 0%, #7dd3fc 50%, #a6b7b9ff 100%)",
-            backgroundClip: "text",
-            color: "transparent",
+            lineHeight: 1.1,
+            letterSpacing: '-0.4',
+            fontSize: "clamp(36px, 5.5vw, 64px)",
+            fontWeight: 700
           }}
         >
-          AI
-        </span>{" "}
-        to help you create{" "}
-        <span
-          style={{
-            display: "inline-block",
-            fontWeight: "bold",
-            fontSize: 60,
-            background:
-              "linear-gradient(90deg, #8ba9d1ff 0%, #7dd3fc 50%, #b7c8c9ff 100%)",
-            backgroundClip: "text",
-            color: "transparent",
-          }}
-        >
-          effective flashcard
-        </span>{" "}
-        decks for learning.
-      </Text>
-      <Group justify="space-between">
-        <Button variant="light" my={50} color="cyan" size="lg" radius={"md"} onClick={authenticateUser}>
-          Want to try it? Sign in and start!
-        </Button>
-        <Button variant="light" my={50} color="grape" size="lg" radius="md">
-          Don't know what you getting into? Learn more! ➡️
-        </Button>
-      </Group>
+          A web platform that leverages{" "}
+          <Text
+            span
+            variant="gradient"
+            gradient={{ from: '#06b6d4', to: '#3b82f6', deg: 90 }}
+            style={{
+              fontWeight: 900,
+              fontSize: '1.2em',
+              display: 'inline-block'
+            }}
+          >AI</Text>{" "}
+          to help you create{" "}
+          <Text
+            span
+            variant="gradient"
+            gradient={{ from: '#06b6d4', to: '#3b82f6', deg: 90 }}
+            style={{
+              fontWeight: 900,
+              fontSize: '1.2em',
+              display: 'inline-block'
+            }}
+          >
+            effective flashcards
+          </Text>{" "} for learning.
+        </Title>
+
+        <Text c="dimmed" fz="lg" maw={720}>
+          Paste your resources, pick a few options, and let it generate flashcards for you! And export it anywhere you want.
+        </Text>
+
+        <Group gap="md" justify="center" wrap="wrap">
+          <Button
+            variant="filled"
+            color="green"
+            size="lg"
+            radius="md"
+            onClick={authenticateUser}
+          >
+            Convinced? Let's get started!
+          </Button>
+
+          <Anchor href="/learn-more" underline="never">
+            <Button variant="light" color="blue" size="lg" radius={"md"}>
+              Learn more ➜
+            </Button>
+          </Anchor>
+        </Group>
+
+
+          <Text c="dimmed" fz="sm">
+            No credit card required. Free forever & open sourced! 🚀
+          </Text>
+
+      </Stack>
     </Container>
   );
 }
