e3d37ec5ca7e011ddf396240f8dc16d25b26005f
xndadelin <adelinmihai071@gmail.com>
2025-10-22 22:49:09 +0300

    GET contest server action.

---
+++ src/app/api/decks/[id]/route.ts
@@ -0,0 +1,61 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+export async function GET(req: NextRequest, 
+    { params } : { params: { id: string } }
+) {
+    try {
+        const cookieStore = cookies();
+        const supabase = createClient(cookieStore);
+
+        const {
+            data: { user },
+        } = await supabase.auth.getUser();
+
+        if (!user) {
+            return NextResponse.json({
+                error: 'Unauthorized'
+            }, { status: 401 })
+        }
+
+        const { data: deck, error: deckError } = await supabase.from('decks').select('*').eq('id', params.id).single();
+
+        if(deckError || !deck) {
+            return NextResponse.json({
+                error: 'deck not found'
+            }, {
+                status: 404
+            })
+        }
+
+        if(deck.owner !== user.id && !deck.is_public) {
+            return NextResponse.json({
+                error: 'Forbidden'
+            }, {
+                status: 403
+            })
+        }
+
+        const { data: cards, error: cardsError } = await supabase.from('cards').select('*').eq('deck_id', params.id).order('position', {
+            ascending: true
+        })
+
+        if(cardsError) {
+            return NextResponse.json({
+                error: 'Error fetching cards',
+            }, { status: 500 })
+        }
+
+        return NextResponse.json({
+            ...deck,
+            cards: cards || []
+        })
+
+    } catch (error) {
+        return NextResponse.json({
+            error: 'Internal Server Error',
+            status: 500
+        })
+    }
+}
