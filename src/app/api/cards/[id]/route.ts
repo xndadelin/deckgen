c3093a999c977d5068dfdba8f1b4b70a7e3d8dda
xndadelin <adelinmihai071@gmail.com>
2025-10-22 23:26:36 +0300

    PATCH card server action.

---
+++ src/app/api/cards/[id]/route.ts
@@ -0,0 +1,68 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (!user) {
+      return NextResponse.json({ error: "unauthorized" }, { status: 401 });
+    }
+    const body = await request.json();
+    const { front, back } = body;
+
+    const { data: card, error: cardError } = await supabase
+      .from("cards")
+      .select("deck_id, decks!inner(owner)")
+      .eq("id", params.id)
+      .single();
+
+    if (cardError || !card) {
+      return NextResponse.json(
+        { error: cardError?.message || "card not found" },
+        {
+          status: 404,
+        }
+      );
+    }
+    if (card.decks && card.decks[0]?.owner) {
+      return NextResponse.json({ error: "forbidden" }, { status: 403 });
+    }
+
+    const { data: updatedCard, error: updateError } = await supabase
+      .from("cards")
+      .update({
+        front,
+        back,
+      })
+      .eq("id", params.id)
+      .select()
+      .single();
+
+    if (updateError) {
+      return NextResponse.json(
+        { error: updateError.message },
+        {
+          status: 500,
+        }
+      );
+    }
+
+    return NextResponse.json({ card: updatedCard }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: "failed to update card",
+      },
+      { status: 500 }
+    );
+  }
+}
